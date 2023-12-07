2eea1b6a11d09658b87be0a8f390532de4fb07fb
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-07 09:56:32 -0600

    Determinar si un numero es par o impar

---
+++ Estructuras Condicionales/Ejercicio 3/Numero par o impar.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n1;
+	
+	cout<<"Ingrese un numero:";
+	cin>>n1;
+	
+	if(n1%2==0){
+		cout<<"El numero es par";
+	}
+	else{
+		cout<<"El numero es impar";
+	}
+	
+	return 0;
+}
