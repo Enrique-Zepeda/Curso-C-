7d442922944840f3bd3d3456a8141da255f2be01
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2023-12-08 21:46:14 -0600

    Ejercicio 4 y 5

---
+++ Estructuras Condicionales/Ejercicio 4/Numero positivo o negativo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a;
+	
+	cout<<"Ingresa un numero: ";
+	cin>>a;
+	
+	if(a>=0){
+		cout<<"El numero es positivo";
+	}
+	else{
+		cout<<"El numero es negativo";
+	}
+	
+	
+	return 0;
+}
